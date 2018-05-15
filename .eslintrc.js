95da1c32330d8e97c9d8204f317effc2b376717f
rtkd <null@no.tld>
2018-05-16 01:44:51 +0200

    Turn off complexity warning.

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,7 +46,7 @@ module.exports = {
             "error",
             "last"
         ],
-        "complexity": "error",
+        "complexity": "off",
         "computed-property-spacing": [
             "error",
             "never"
